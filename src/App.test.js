41a482b1a445de74b1d520505f5c6c6306548dc0
benitogf <benitogf@outlook.com>
2019-08-10 19:06:50 +0800

    rf

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Locks from './Locks';
+import Locks from './dashboard/Locks';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
